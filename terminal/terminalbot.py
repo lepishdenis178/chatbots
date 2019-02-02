5398ea20595e454cad8b9ecb9ae6eadd3bf78352
lepishdenis178 <meomornie@tuta.io>
2019-02-03 01:13:13 +0300

    new

--- terminal/terminalbot.py
+++ terminal/terminalbot.py
@@ -1,14 +1,25 @@
-howmuch = int(input('сколько есть у бота:  '))
+import random
+howmuch = 0
+answeryes = ['+', 'угу', 'да', 'ага', ')']
+answerno = ['нет', 'не', '-', '(', 'неа']
+answerhello = ['привет', 'здарова', 'йо', 'ку']
 while True:
     inputvalue = input('msg: ')
 
     checkvalue = howmuch == 0
     if checkvalue == True:
-        answer='ans: не'
+        answer=random.choice(answerno)
     else:
-        answer='ans: +'
+        answer=random.choice(answeryes)
 
     if inputvalue == 'есть че?':
         print(answer)
+    elif inputvalue == 'есть че':
+        print(answer)
+    elif inputvalue == 'есть?':
+        print(answer)
     elif inputvalue == 'привет':
-        print('ans: привет')
+        print(random.choice(answerhello))
+    elif inputvalue == '***дать 1':
+        howmuch = howmuch + 1
+        print('msg: спс')
